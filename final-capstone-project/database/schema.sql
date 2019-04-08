d984b4d62a5c10d1c6cf54463b4c39631f257568
Ty <ty.william.owen@gmail.com>
2019-04-08 17:25:49 -0400

    TY: rebuilt database after getting the CSV file from Brian

--- final-capstone-project/database/schema.sql
+++ final-capstone-project/database/schema.sql
@@ -5,38 +5,42 @@
 BEGIN;
 
 -- CREATE statements go here
-DROP TABLE IF EXISTS app_user;
-DROP TABLE IF EXISTS survey;
-DROP TABLE IF EXISTS question;
-DROP TABLE IF EXISTS survey_question;
-
-CREATE TABLE app_user (
-  id SERIAL PRIMARY KEY,
-  user_name varchar(32) NOT NULL UNIQUE,
-  password varchar(32) NOT NULL,
-  role varchar(32),
-  salt varchar(255) NOT NULL
-);
 
 create table survey 
 (survey_id serial PRIMARY KEY, 
-survey_date TIMESTAMP NOT NULL
+survey_date text NOT NULL,
+room text NOT NULL
 ); 
+
+
+create table answer
+(answer_id serial PRIMARY KEY, 
+question_id int NOT NULL, 
+answer_text text, 
+student_id text NOT NULL, 
+survey_id int NOT NULL
+); 
+
 
 create table question
 (question_id serial PRIMARY KEY, 
-survey_id int NOT NULL, 
-question_number int NOT NULL, 
-question_text text NOT NULL, 
-question_answer text NOT NULL
+question_text text NOT NULL 
 ); 
 
 
-create table survey_question(
+create table student
+(student_id text PRIMARY KEY, 
+student_first_name text NOT NULL, 
+student_last_name text NOT NULL
+); 
+
+
+create table question_answer(
 question_id int NOT NULL, 
-survey_id int NOT NULL, 
+answer_id int NOT NULL, 
 constraint fk_survey_question_question_id foreign key (question_id) references question(question_id), 
-constraint fk_survey_question_survey_id foreign key (survey_id) references survey(survey_id)
+constraint fk_survey_question_answer_id foreign key (answer_id) references answer(answer_id)
 );
+
 
 COMMIT;
