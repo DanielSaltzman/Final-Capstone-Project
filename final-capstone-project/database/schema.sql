024e337aacfe16174b9de47e266112fb2ceb11dd
Ty <ty.william.owen@gmail.com>
2019-04-08 18:22:57 -0400

    Ty: finished building database and adding dummy data for all of the rows. There should be enough for you both to manipulate tomorrow

--- final-capstone-project/database/schema.sql
+++ final-capstone-project/database/schema.sql
@@ -5,13 +5,26 @@
 BEGIN;
 
 -- CREATE statements go here
+drop table if exists question_answer;
+DROP TABLE IF EXISTS app_user; 
+drop table if exists survey; 
+drop table if exists answer;
+drop table if exists question; 
+drop table if exists student; 
+
+CREATE TABLE app_user (
+  id SERIAL PRIMARY KEY,
+  user_name varchar(32) NOT NULL UNIQUE,
+  password varchar(32) NOT NULL,
+  role varchar(32),
+  salt varchar(255) NOT NULL
+);
 
 create table survey 
 (survey_id serial PRIMARY KEY, 
 survey_date text NOT NULL,
 room text NOT NULL
 ); 
-
 
 create table answer
 (answer_id serial PRIMARY KEY, 
@@ -21,19 +34,16 @@ student_id text NOT NULL,
 survey_id int NOT NULL
 ); 
 
-
 create table question
 (question_id serial PRIMARY KEY, 
 question_text text NOT NULL 
 ); 
-
 
 create table student
 (student_id text PRIMARY KEY, 
 student_first_name text NOT NULL, 
 student_last_name text NOT NULL
 ); 
-
 
 create table question_answer(
 question_id int NOT NULL, 
