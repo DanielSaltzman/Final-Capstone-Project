efadb492c23628d1d774f31e0918e96f15269192
Ty <ty.william.owen@gmail.com>
2019-04-09 18:50:53 -0400

    created answer DAO and JDBCAnswerDAO

---
+++ final-capstone-project/src/main/java/com/techelevator/model/AnswerDAO.java
@@ -0,0 +1,8 @@
+package com.techelevator.model;
+
+import java.util.List;
+
+public interface AnswerDAO {
+
+	public List<Answer> getAnswersBySurveyIdAndQuestionId(int surveyId, int question_id); 
+}
