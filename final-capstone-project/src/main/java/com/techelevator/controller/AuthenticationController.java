d8389b5f170514363f490d6e5fab4cded5b257c4
quinnsprouse <quinnsprouse@gmail.com>
2019-04-15 09:49:33 -0400

    added One-Time Password Field

--- final-capstone-project/src/main/java/com/techelevator/controller/AuthenticationController.java
+++ final-capstone-project/src/main/java/com/techelevator/controller/AuthenticationController.java
@@ -242,6 +242,20 @@ public class AuthenticationController {
 		
 	}
 	
+//	@RequestMapping(path="/changePassword", method=RequestMethod.POST) 
+//	public String changePassword(@RequestParam String userName, @RequestParam String password, HttpSession session, ModelMap model) {
+//		
+//		userDAO.updatePassword(userName, password);
+//		
+//		User user = ((User) session.getAttribute("currentUser"));
+//		
+//		logDao.inserLog(user.getUserName(), "User Changed Password");
+//		
+//		
+//		return "redirect:/login";
+//		
+//	}
+	
 
 	@RequestMapping(path="/logout", method=RequestMethod.POST)
 	public String logout(ModelMap model, HttpSession session) {
